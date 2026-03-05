2540cc0fb17bb9a36639af9c62d12748224e2cf8
badr <badirov.usa@gmail.com>
2026-03-05 06:54:22 +0000

    fix vit

---
+++ dist/server/__vite_rsc_assets_manifest.js
@@ -0,0 +1,312 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-BWqZzo-y.js\")",
+  "clientReferenceDeps": {
+    "dbcccf1a58c6": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "76755c067718": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "8b5dbfc589c1": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "35390e5f162d": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "4fc0c9d331c8": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "b0bebd5b6373": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "8735c7ab1908": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "138e38521ea4": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "2d16275a9da9": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "079b62ebd226": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "9a68752807da": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "09df11997058": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "cd84e95f679a": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "5916afaeeaec": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "341666f078f7": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "7f3d9b29dfe4": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "1f7d554b4a57": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "667004486e38": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "286851f3a085": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "7e597dcf4fd2": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "8defb14935e4": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "f084f6afaaef": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "5fe8ff67a435": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "aff943ef2b65": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "85a6e87e5c78": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "f359f9c47193": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "bdb052a6ee9c": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "09cf9e0cca7a": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "1d10b2d06bb7": {
+      "js": [
+        "/assets/index-BWqZzo-y.js",
+        "/assets/framework-dxBPwwpX.js"
+      ],
+      "css": []
+    },
+    "f29e6e234fea": {
+      "js": [
+        "/assets/worker-entry-DJUNs-Q7.js",
+        "/assets/framework-dxBPwwpX.js",
+        "/assets/index-BWqZzo-y.js"
+      ],
+      "css": []
+    },
+    "0deffcb8ffd7": {
+      "js": [
+        "/assets/worker-entry-DJUNs-Q7.js",
+        "/assets/framework-dxBPwwpX.js",
+        "/assets/index-BWqZzo-y.js"
+      ],
+      "css": []
+    },
+    "c2747888630f": {
+      "js": [
+        "/assets/worker-entry-DJUNs-Q7.js",
+        "/assets/framework-dxBPwwpX.js",
+        "/assets/index-BWqZzo-y.js"
+      ],
+      "css": []
+    },
+    "95cf9d25c5b0": {
+      "js": [
+        "/assets/worker-entry-DJUNs-Q7.js",
+        "/assets/framework-dxBPwwpX.js",
+        "/assets/index-BWqZzo-y.js"
+      ],
+      "css": []
+    },
+    "7d3ad4055eb6": {
+      "js": [
+        "/assets/worker-entry-DJUNs-Q7.js",
+        "/assets/framework-dxBPwwpX.js",
+        "/assets/index-BWqZzo-y.js"
+      ],
+      "css": []
+    },
+    "b58f9b9bb6a4": {
+      "js": [
+        "/assets/worker-entry-DJUNs-Q7.js",
+        "/assets/framework-dxBPwwpX.js",
+        "/assets/index-BWqZzo-y.js"
+      ],
+      "css": []
+    },
+    "de2917a3ab24": {
+      "js": [
+        "/assets/worker-entry-DJUNs-Q7.js",
+        "/assets/framework-dxBPwwpX.js",
+        "/assets/index-BWqZzo-y.js"
+      ],
+      "css": []
+    },
+    "d96d33eba807": {
+      "js": [
+        "/assets/worker-entry-DJUNs-Q7.js",
+        "/assets/framework-dxBPwwpX.js",
+        "/assets/index-BWqZzo-y.js"
+      ],
+      "css": []
+    },
+    "4b21b0276245": {
+      "js": [
+        "/assets/worker-entry-DJUNs-Q7.js",
+        "/assets/framework-dxBPwwpX.js",
+        "/assets/index-BWqZzo-y.js"
+      ],
+      "css": []
+    },
+    "ab9335e7f626": {
+      "js": [
+        "/assets/worker-entry-DJUNs-Q7.js",
+        "/assets/framework-dxBPwwpX.js",
+        "/assets/index-BWqZzo-y.js"
+      ],
+      "css": []
+    },
+    "b0e839616971": {
+      "js": [
+        "/assets/worker-entry-DJUNs-Q7.js",
+        "/assets/framework-dxBPwwpX.js",
+        "/assets/index-BWqZzo-y.js"
+      ],
+      "css": []
+    },
+    "cf9a10ca1a61": {
+      "js": [
+        "/assets/worker-entry-DJUNs-Q7.js",
+        "/assets/framework-dxBPwwpX.js",
+        "/assets/index-BWqZzo-y.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "src/app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-DI0k6okP.css"
+      ]
+    }
+  }
+}
