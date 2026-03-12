abaf1da90b7d590a15a03a746d24eff3d2ee65d2
propre <badirov.usa@gmail.com>
2026-03-12 21:02:46 +0000

    remove popunder

--- src/components/global-ads.tsx
+++ src/components/global-ads.tsx
@@ -4,17 +4,6 @@ import { usePathname } from "next/navigation";
 import Script from "next/script";
 
 export function GlobalAds() {
-  const pathname = usePathname();
-
-  // Only show popunder on the success page
-  if (pathname === "/success") {
-    return (
-      <Script 
-        src="https://pl28887136.effectivegatecpm.com/46/71/de/4671deb8ab4d438cae52337aaa9ddb77.js" 
-        strategy="afterInteractive" 
-      />
-    );
-  }
-
+  // All popunder scripts removed per user request
   return null;
 }
