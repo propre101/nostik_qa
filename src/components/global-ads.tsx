3f72009d491094fb0f457d762a3fab4cd93648b5
propre <badirov.usa@gmail.com>
2026-03-12 21:13:30 +0000

    restore popunder

--- src/components/global-ads.tsx
+++ src/components/global-ads.tsx
@@ -4,6 +4,17 @@ import { usePathname } from "next/navigation";
 import Script from "next/script";
 
 export function GlobalAds() {
-  // All popunder scripts removed per user request
+  const pathname = usePathname();
+
+  // Only show popunder on the success page
+  if (pathname === "/success") {
+    return (
+      <Script 
+        src="https://authoritieswoundjoint.com/46/71/de/4671deb8ab4d438cae52337aaa9ddb77.js" 
+        strategy="afterInteractive" 
+      />
+    );
+  }
+
   return null;
 }
