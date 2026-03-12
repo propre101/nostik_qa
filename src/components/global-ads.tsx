43627acb53b9aa6c9e959da933efecec3c1511d5
propre <badirov.usa@gmail.com>
2026-03-12 19:10:27 +0000

    test

--- src/components/global-ads.tsx
+++ src/components/global-ads.tsx
@@ -6,20 +6,15 @@ import Script from "next/script";
 export function GlobalAds() {
   const pathname = usePathname();
 
-  // Only show ads and detectors on the main page and success page
-  if (pathname !== "/" && pathname !== "/success") {
-    return null;
+  // Only show popunder on the success page
+  if (pathname === "/success") {
+    return (
+      <Script 
+        src="https://pl28887136.effectivegatecpm.com/46/71/de/4671deb8ab4d438cae52337aaa9ddb77.js" 
+        strategy="afterInteractive" 
+      />
+    );
   }
 
-  return (
-    <>
-      {/* Popunder script ONLY on success page */}
-      {pathname === "/success" && (
-        <Script 
-          src="https://pl28887136.effectivegatecpm.com/46/71/de/4671deb8ab4d438cae52337aaa9ddb77.js" 
-          strategy="afterInteractive" 
-        />
-      )}
-    </>
-  );
+  return null;
 }
